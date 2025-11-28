2844fe1cafd78e5dd26d8a22be3ca02c8b52c274
Jordan Trakas <jtrakas57@gmail.com>
2025-11-28 13:15:01 -0500

    Adding docstring to function

--- Start/1. PythonFeatures/doc_strings.py
+++ Start/1. PythonFeatures/doc_strings.py
@@ -2,12 +2,20 @@
 # Demonstrate the use of documentation strings
 
 
-def myFunction(arg1, arg2=None):
+def my_function(arg1, arg2=None):
+    """
+    :param arg1: This parameter is the first parameter
+    :param arg2: This parameter is the second parameter
+    :return: Prints out the two parameters and returns None
+    :raises TypeError: If arg1 is not a string
+    Example >>> my_function(1, 2)
+    1 2
+    """
     print(arg1, arg2)
 
 
 def main():
-    print(myFunction.__doc__)
+    print(my_function.__doc__)
 
 
 if __name__ == "__main__":
