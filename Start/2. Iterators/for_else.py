6f3e565ad81e857413236cb558aa0b1f3464b789
Jordan Trakas <jtrakas57@gmail.com>
2025-11-28 16:58:16 -0500

    Modified for_else.py concerning for loop with else clause for if for loop completes without interruption

--- Start/2. Iterators/for_else.py
+++ Start/2. Iterators/for_else.py
@@ -4,16 +4,26 @@
 names = ["Jim", "Pam", "Creed", "Michael", "Dwight", "Oscar", "Kevin", "Phyllis"]
 
 # The else clause on a for loop is only executed if the loop completes every iteration
-def findname(target):
+def find_name(target):
     for name in names:
         if name == target:
-            print("Name found");
+            print("Name found")
             return True
-    
-    print("Name not found")
-    return False
+    else:
+        print("Name not found")
+        return False
 
-print(findname("Creed"))
-print(findname("Tom"))
+# print(find_name("Creed"))
+# print(find_name("Tom"))
 
 # Check if a number is prime
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+
+is_prime(31)
+is_prime(56)
