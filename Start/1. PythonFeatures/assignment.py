4541ebf028f4a1f28da041832a3bcc722c8f492d
Jordan Trakas <jtrakas57@gmail.com>
2025-11-28 14:44:03 -0500

    Modified assignment.py file showing how to use walrus operator (:=) in expressions

--- Start/1. PythonFeatures/assignment.py
+++ Start/1. PythonFeatures/assignment.py
@@ -5,21 +5,22 @@ import pprint
 
 
 # regular assignment statements assign a value
-x = 5
-print(x)
+# x = 5
+# print(x)
 
 # the assignment operator is part of an expression
-
+# (x := 10)
+# print(x)
 
 # The assignment expression is useful for writing concise code
-
+# while (the_string := input("Enter a string: ")) != "exit":
+#     print(the_string)
 
 # The walrus operator can help reduce redundant function calls
 values = [12, 0, 10, 5, 9, 18, 41, 23, 30, 16, 18, 9, 18, 22]
-l = len(values)
-s = sum(values)
 val_data = {
-    "length": l,
-    "total": s,
+    "length": (l := len(values)),
+    "total": (s := sum(values)),
     "average": s/l
 }
+pprint.pprint(val_data)
