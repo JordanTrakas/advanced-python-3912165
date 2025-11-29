95d08163f7d41fa30a37d717c6edb837d291c16e
Jordan Trakas <jtrakas57@gmail.com>
2025-11-29 11:43:33 -0500

    Modified sequence_compare.py concerning comparing sequences and using itertools and the all() function to compare.

--- Start/4. Sequences/sequence_compare.py
+++ Start/4. Sequences/sequence_compare.py
@@ -5,21 +5,25 @@ import itertools
 
 
 # define some lists
-seq1 = [1, 2, 3, 6, 10, 15, 34, 56]
-seq2 = [1, 2, 5, 7, 9, 18, 22, 38, 91]
+seq1 = [1, 2, 3, 4, 5, 6, 7]
+seq2 = [100, 1, 1, 1, 1, 1, 1]
 
 # define a tuple
-seq3 = (1, 2, 3, 6, 10, 15, 34, 56)
+seq3 = (1, 2, 3, 4, 5, 6, 7)
 
 # compare the sequences
-
+# print(seq1 == seq2)
+# print(seq1 > seq2)
+# print(seq1 < seq2)
 
 # sequences that have equal values but different number of items:
-seq4 = [10, 20, 30]
-seq5 = [10, 20, 30, 40, 50]
-
+# seq4 = [10, 20, 30]
+# seq5 = [10, 20, 30, None, None]
+# print(seq5 > seq4)
 
 # Sequences must be of the same type to be compared
-
+# print(seq1 > list(seq3))
 
 # use the all() function to compare two arbitrary sequences
+result = all(x == y for x, y in itertools.zip_longest(seq1, seq3))
+print(result)
