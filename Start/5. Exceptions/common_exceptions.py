252488315e0c1668220e62dbef478cbec5c6419a
Jordan Trakas <jtrakas57@gmail.com>
2025-11-29 23:53:10 -0500

    Modified common_exceptions.py concerning various exception types in Python.

--- Start/5. Exceptions/common_exceptions.py
+++ Start/5. Exceptions/common_exceptions.py
@@ -2,16 +2,20 @@
 # Understanding the built-in exception classes in Python
 
 # IndexError occurs when you try to access an index that is out of range
-int_list = [0,3,6,1,8,7,3,5]
-
+# int_list = [0,3,6,1,8,7,3,5]
+# print(int_list["A"]) # Raises a TypeError
 
 # KeyError is similar - it is raised when a key is not found in a Dictionary
-my_dict = {1: "one", 2: "two", 3: "three"}
-
+# my_dict = {1: "one", 2: "two", 3: "three"}
+# print(my_dict[4]) # Raises a KeyError because 4 is not a valid key
 
 # FileNotFoundError is raised when you try to access a file that doesn't exist
-
+# with open("myfile.txt", "r") as f:
+#     print(f)
 
 # FileExistsError is raised when a file or directory already exists
+import os
+# os.mkdir("testdir")
 
 # NotADirectoryError is raised when try to perform a dir operation on a non-dir object
+os.listdir("myfile.txt")
