4a073b831dd1683e78a9cc196bf57158fb84d301
Jordan Trakas <jtrakas57@gmail.com>
2025-11-29 23:23:57 -0500

    Created code_challenge_sequences.py concerning the set comprehension to output unique punctuation characters in a string.

---
+++ Start/4. Sequences/code_challenge_sequences.py
@@ -0,0 +1,10 @@
+# I am given a unique set of characters and need to find a set of all the unique punctuation characters in the string
+
+import string
+
+def unique_characters(s):
+    unique_chars = {c for c in s if c in string.punctuation}
+    return unique_chars
+
+test_string = "The quick, brown fox: jumps over the lazy dog! Dog not amused."
+print(unique_characters(test_string))
