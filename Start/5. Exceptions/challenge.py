c4456525bb9caf303b3802c7dff5c5e03d2304a1
Jordan Trakas <jtrakas57@gmail.com>
2025-11-30 00:24:06 -0500

    Attempted challenge.py concerning raising custom exceptions.

--- Start/5. Exceptions/challenge.py
+++ Start/5. Exceptions/challenge.py
@@ -2,7 +2,15 @@
 # Programming challenge for working with Exceptions
 
 # Implement the InvalidTempError exception class here
-
+class InvalidTempError(Exception):
+    """Raised when the temperature is too cold or too hot"""
+    def __init__(self, temp):
+        self.temp = temp
+        if temp < 100:
+            self.message = f"Oven is not hot enough."
+        elif temp > 500:
+            self.message = f"Warning! Oven is too hot!"
+        super().__init__(self.message)
 
 class DigitalOven:
     def __init__(self):
@@ -10,6 +18,10 @@ class DigitalOven:
 
     def set_temp(self, temp):
         self.temp = temp
+        if 0 < temp < 100:
+            raise InvalidTempError(temp)
+        elif temp > 500:
+            raise InvalidTempError(temp)
 
     def get_temp(self):
         return self.temp
@@ -18,8 +30,10 @@ def test_oven(test_temp):
     global oven
     try:
         oven.set_temp(test_temp)
+    except InvalidTempError as e:
+        print(e)
     finally:
-        print(f"Current temp setting is {oven.get_temp()}")
+        print(f"Current temp setting is {oven.get_temp()}\n")
 
 # An "InvalidTempError" Exception should be raised if the temperature
 # is set below 100 degrees or above 500 degrees
