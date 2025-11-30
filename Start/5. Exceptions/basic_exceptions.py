36f7f2920eb6ecacb1f8d16cb0032586bc1c0183
Jordan Trakas <jtrakas57@gmail.com>
2025-11-29 23:46:27 -0500

    Modified basic_exceptions.py concerning basic exceptions in Python.

--- Start/5. Exceptions/basic_exceptions.py
+++ Start/5. Exceptions/basic_exceptions.py
@@ -2,9 +2,17 @@
 # Working with basic exception handling
 
 # Try to execute some code that might cause an exception:
-
-num = input("Enter the first number: ")
-denom = input("Enter the second numnber: ")
-n = int(num)
-d = int(denom)
-print(n/d)
+try:
+    n = int(input("Enter the first number: "))
+    d = int(input("Enter the second number: "))
+    result = n/d
+except ZeroDivisionError as e:
+    print("Cannot divide by zero")
+    print(f"Exception: {e}")
+except ValueError as e:
+    print("Did not receive a valid number")
+    print(f"Exception: {e}")
+else:
+    print(f"Result: {result}")
+finally:
+    print("Thanks for playing!")
